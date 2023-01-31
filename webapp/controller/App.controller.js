812015bf1e2f524cc7f6a3f70ac1a2bf417c0ea2
Kirill <kirisakov4@yandex.ru>
2023-01-31 12:04:39 +0300

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,12 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'], (Controller) => {
-  'use strict'
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller', 'sap/m/MessageToast'],
+  (Controller, MessageToast) => {
+    'use strict'
 
-  return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
-    onClickHello: () => {
-      alert('Hello World!')
-    },
-  })
-})
+    return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
+      onClickHello: () => {
+        MessageToast.show('Hello World!')
+      },
+    })
+  }
+)
