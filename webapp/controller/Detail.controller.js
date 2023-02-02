6ef4e4e17d6ecb7fe52646e0e58f8c5c2096487b
Kirill <kirisakov4@yandex.ru>
2023-02-02 16:33:22 +0300

    Step 32: Routing Back and History

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,21 +1,35 @@
-sap.ui.define(['sap/ui/core/mvc/Controller'], (Controller) => {
-  'use strict'
-  return Controller.extend('sap.ui.demo.walkthrough.controller.Detail', {
-    onInit: function () {
-      const oRouter = this.getOwnerComponent().getRouter()
-      oRouter
-        .getRoute('detail')
-        .attachPatternMatched(this._onObjectMatched, this)
-    },
-    _onObjectMatched: function (oEvent) {
-      this.getView().bindElement({
-        path:
-          '/' +
-          window.decodeURIComponent(
-            oEvent.getParameter('arguments').invoicePath
-          ),
-        model: 'invoice',
-      })
-    },
-  })
-})
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller', 'sap/ui/core/routing/History'],
+  (Controller, History) => {
+    'use strict'
+    return Controller.extend('sap.ui.demo.walkthrough.controller.Detail', {
+      onInit: function () {
+        const oRouter = this.getOwnerComponent().getRouter()
+        oRouter
+          .getRoute('detail')
+          .attachPatternMatched(this._onObjectMatched, this)
+      },
+      _onObjectMatched: function (oEvent) {
+        this.getView().bindElement({
+          path:
+            '/' +
+            window.decodeURIComponent(
+              oEvent.getParameter('arguments').invoicePath
+            ),
+          model: 'invoice',
+        })
+      },
+      onNavBack: function () {
+        const oHistory = History.getInstance()
+        const sPreviousHash = oHistory.getPreviousHash()
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1)
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter()
+          oRouter.navTo('overview', {}, true)
+        }
+      },
+    })
+  }
+)
