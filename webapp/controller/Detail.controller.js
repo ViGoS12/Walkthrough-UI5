f233350bd94c554944ffa2aa835c88f6024b46a4
Kirill <kirisakov4@yandex.ru>
2023-02-02 16:56:55 +0300

    Step 33: Custom Controls

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/ui/core/routing/History'],
-  (Controller, History) => {
+  [
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/core/routing/History',
+    'sap/m/MessageToast',
+  ],
+  (Controller, History, MessageToast) => {
     'use strict'
     return Controller.extend('sap.ui.demo.walkthrough.controller.Detail', {
       onInit: function () {
@@ -10,6 +14,7 @@ sap.ui.define(
           .attachPatternMatched(this._onObjectMatched, this)
       },
       _onObjectMatched: function (oEvent) {
+        this.byId('rating').reset()
         this.getView().bindElement({
           path:
             '/' +
@@ -29,6 +34,17 @@ sap.ui.define(
           const oRouter = this.getOwnerComponent().getRouter()
           oRouter.navTo('overview', {}, true)
         }
+      },
+
+      onRatingChange: function (oEvent) {
+        const fValue = oEvent.getParameter('value')
+        const oResourceBundle = this.getView()
+          .getModel('i18n')
+          .getResourceBundle()
+
+        MessageToast.show(
+          oResourceBundle.getText('ratingConfirmation', [fValue])
+        )
       },
     })
   }
